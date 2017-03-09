76a8a2ff88fa27e162f44056ec70f1e39f1c509e
Fabricio Catae <fcatae@microsoft.com>
2017-03-09 02:01:20 -0300

    wip

---
+++ task.fsx
@@ -0,0 +1,35 @@
+
+#r "System.Xml.Linq.dll"
+open System.Xml.Linq
+open System.Linq
+
+let FILE_WORDPRESS_XML = "input\\sample.xml"
+
+let doc = XDocument.Load FILE_WORDPRESS_XML 
+
+let de = doc.Elements()
+
+de.First().
+
+
+let xName = XName.Get
+
+de.Descendants(xName "item") 
+|> Seq.map(fun (x: XElement)-> x.Elements(xName "title").First().Name )
+
+let (a:XElement) = null
+a.
+
+let ShowNames (els: seq<XElement>) = els |> Seq.map(fun x-> x.Name.ToString() )
+
+de.Elements(xName "rss")  |>  ShowNames
+;;
+
+let GetChild name (elx: XElement) = elx.Elements(xName name)
+
+let (many: XElement seq) = de.First().Elements().First().Elements() 
+many.Count()
+
+de.
+
+
