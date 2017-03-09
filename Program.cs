e65b02eec57c94d76a266b80c8a8e6f19b6f1b29
Fabricio Catae <fcatae@microsoft.com>
2017-03-09 00:12:08 -0300

    C# program

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace sqlninja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FILE_WORDPRESS_XML = "sqlblogninja.wordpress.xml";
+
+            Console.WriteLine("Hello World!");
+
+            var document = XDocument.Load(FILE_WORDPRESS_XML);
+            
+            
+        }
+    }
+}
