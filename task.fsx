9f1f45a1863f2d7f7d873f9b637c7cb7b42a27d1
Fabricio Catae <fcatae@microsoft.com>
2017-03-10 00:35:27 -0300

    finish the task.fsx

--- task.fsx
+++ task.fsx
@@ -13,12 +13,23 @@ let GetChild (item: XElement) name = item.Element(xmlNS name).Value
 
 // Verify if the item is a post indeed
 let IsPost (item: XElement) = (item.Element(xmlWP "post_type").Value = "post")
+let IsPublished (item: XElement) = (item.Element(xmlWP "status").Value = "publish")
 
 // Just do it!
+let FindTitleLink (item: XElement) = (GetChild item "title" , GetChild item "link" )
+let GenerateHtml (t: string,l: string) = XElement(xmlNS "a", t,
+                                            XAttribute(xmlNS "link", l))
+let TransformHtml (item: XElement) = (FindTitleLink item) |> GenerateHtml
 
-"input\\sqlblogninja.wordpress.xml" 
+let elements = 
+    "input\\sqlblogninja.wordpress.xml" 
     |> XDocument.Load
     |> GetDescendent "item"
     |> Seq.filter( IsPost )
-    |> Seq.map( fun item -> 
-        (GetChild item "title" , GetChild item "link" ) )
+    |> Seq.filter( IsPublished )
+    |> Seq.map( TransformHtml )
+
+XDocument( 
+    XElement(xmlNS "posts", elements ))
+    |> fun doc -> doc.Save "output\\postlist.xml"
+
