0248d7feb777b26e6124af35b77f0c6026ede08f
Fabricio Catae <fcatae@microsoft.com>
2017-03-13 22:55:23 -0300

    generate readme

--- src/wpreader.fsx
+++ src/wpreader.fsx
@@ -2,6 +2,7 @@
 
 open System.Xml.Linq
 open System.Linq
+open System.IO
 
 type WPReaderBlog (node: XElement) =
 
@@ -11,16 +12,21 @@ type WPReaderBlog (node: XElement) =
 
     // XML Function Utils    
     let getChild name = node.Element(name).Value
-    let _title = getChild (xmlNS "title")
-    let _link = getChild (xmlNS "link")
+    // let _title = getChild (xmlNS "title")
+    // let _link = getChild (xmlNS "link")
     
     // Methods
     member this.IsPost = (node.Element(xmlWP "post_type").Value = "post")
     member this.IsPublished = (node.Element(xmlWP "status").Value = "publish")
-    member this.Title = _title
-    member this.Link = _link
+    member this.Title = getChild (xmlNS "title")
+    member this.Link = getChild (xmlNS "link")
 
+    member this.Categories = node.Elements(xmlNS "category")
+                                |> Seq.filter( fun category -> category.Attribute(xmlNS "domain").Value = "category" )
+                                |> Seq.map( fun category -> category.Value )
+                                |> Seq.toArray
 
+//<category domain="category" nicename="sql"><![CDATA[SQL]]></category>
 
 type WPReader()= 
 
@@ -42,33 +48,33 @@ type WPReader()=
 
 
 
-(*
-
-
-let wp = WPReader("input\\sample.xml")
-let (wp1 ) = wp.Process() 
-                            |> Seq.filter( fun post -> post.IsPost && post.IsPublished )
-
-let GetXmlNodes name (doc: XDocument) = doc.Descendants(xmlNS name)
-
-let arr ="input\\sample.xml"
-        |> XDocument.Load
-        |> GetXmlNodes "item"
-        |> Seq.take 1
-        |> Seq.toArray
-
-let l = WPReader.CreateBlogInfo arr.[0]
-
-l.IsPost
-
-|> Seq.map( WPReader.CreateBlogInfo )
-*)
-
-
 let elements = 
     "input\\sqlblogninja.wordpress.xml" 
     |> WPReader.LoadPosts
     |> Seq.filter( fun post -> post.IsPost && post.IsPublished )
-    |> Seq.map( fun post -> (post.Title, post.Link ) )
+    //|> Seq.map( fun post -> (post.Title, post.Link, post.Categories ) )
+    //|> Seq.take 10
+    //|> Seq.toArray
 
+let categoryIndex =
+    seq { for item in elements do
+            for category in item.Categories do
+                yield (category, item)
+    }
+    //|> Seq.filter( fun (cat,item) -> cat <> "SQL" )
+    |> Seq.groupBy( fun (cat,item) -> cat )
+    |> Seq.sortBy( fun (cat,item) -> cat )    
     |> Seq.toArray
+
+let printFile = 
+    let lines = seq {
+        for (category, posts) in categoryIndex do
+            yield sprintf "Category: %s" category
+            yield ""
+            // loop each entry
+            for (_,post) in posts do
+                yield sprintf "- [%s](%s)" post.Title post.Link
+                yield ""
+    }
+    File.WriteAllLines("readme.md", lines)
+    
